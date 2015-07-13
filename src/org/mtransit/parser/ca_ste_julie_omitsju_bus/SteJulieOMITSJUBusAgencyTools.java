905c356a0bf4389c28e9cb06323f42b22e4999f8
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 17:31:26 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_ste_julie_omitsju_bus/SteJulieOMITSJUBusAgencyTools.java
+++ src/org/mtransit/parser/ca_ste_julie_omitsju_bus/SteJulieOMITSJUBusAgencyTools.java
@@ -35,7 +35,7 @@ public class SteJulieOMITSJUBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating OMITSJU bus data...\n");
+		System.out.printf("\nGenerating OMITSJU bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
