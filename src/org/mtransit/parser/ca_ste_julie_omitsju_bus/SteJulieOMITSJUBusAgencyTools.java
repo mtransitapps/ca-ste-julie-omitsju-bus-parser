1857ce9139d1276f843cf36def51d3ed7d92eb72
Mathieu Méa <mathieu.mea@gmail.com>
2017-07-01 11:47:33 -0400

    New URL.

--- src/org/mtransit/parser/ca_ste_julie_omitsju_bus/SteJulieOMITSJUBusAgencyTools.java
+++ src/org/mtransit/parser/ca_ste_julie_omitsju_bus/SteJulieOMITSJUBusAgencyTools.java
@@ -18,8 +18,8 @@ import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.CleanUtils;
 import org.mtransit.parser.mt.data.MTrip;
 
-// https://www.amt.qc.ca/en/about/open-data
-// https://www.amt.qc.ca/xdata/omitsju/google_transit.zip
+// https://rtm.quebec/en/about/open-data
+// https://rtm.quebec/xdata/omitsju/google_transit.zip
 public class SteJulieOMITSJUBusAgencyTools extends DefaultAgencyTools {
 
 	public static void main(String[] args) {
