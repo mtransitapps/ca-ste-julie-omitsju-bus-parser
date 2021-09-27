7216a5bc474381ba568369aebcae336b4dbdd7c4
Mathieu Méa <mathieu.mea@gmail.com>
2021-09-27 18:14:56 -0400

    Cleanup

--- src/main/java/org/mtransit/parser/ca_ste_julie_omitsju_bus/SteJulieOMITSJUBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_ste_julie_omitsju_bus/SteJulieOMITSJUBusAgencyTools.java
@@ -1,23 +1,21 @@
 package org.mtransit.parser.ca_ste_julie_omitsju_bus;
 
+import static org.mtransit.commons.Constants.SPACE_;
+import static org.mtransit.commons.RegexUtils.DIGITS;
+import static org.mtransit.commons.StringUtils.EMPTY;
+
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.mtransit.commons.CharUtils;
 import org.mtransit.commons.CleanUtils;
 import org.mtransit.commons.RegexUtils;
-import org.mtransit.commons.StringUtils;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.MTLog;
-import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.mt.data.MAgency;
 
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static org.mtransit.commons.StringUtils.EMPTY;
-import static org.mtransit.commons.Constants.SPACE_;
 
 // https://exo.quebec/en/about/open-data
 // https://exo.quebec/xdata/omitsju/google_transit.zip
@@ -44,22 +42,19 @@ public class SteJulieOMITSJUBusAgencyTools extends DefaultAgencyTools {
 		return MAgency.ROUTE_TYPE_BUS;
 	}
 
-	private static final long RID_STARTS_WITH_T = 20_000L;
+	@Override
+	public boolean defaultRouteIdEnabled() {
+		return true;
+	}
 
 	@Override
-	public long getRouteId(@NotNull GRoute gRoute) {
-		if (CharUtils.isDigitsOnly(gRoute.getRouteShortName())) {
-			return Long.parseLong(gRoute.getRouteShortName());
-		}
-		//noinspection deprecation
-		final Matcher matcher = DIGITS.matcher(gRoute.getRouteId());
-		if (matcher.find()) {
-			final long id = Long.parseLong(matcher.group());
-			if (gRoute.getRouteShortName().toLowerCase(Locale.ENGLISH).startsWith("t")) {
-				return id + RID_STARTS_WITH_T;
-			}
-		}
-		throw new MTLog.Fatal("Unexpected route ID for %s!", gRoute);
+	public boolean useRouteShortNameForRouteId() {
+		return true;
+	}
+
+	@Override
+	public boolean defaultRouteLongNameEnabled() {
+		return true;
 	}
 
 	@NotNull
@@ -71,44 +66,15 @@ public class SteJulieOMITSJUBusAgencyTools extends DefaultAgencyTools {
 		return CleanUtils.cleanLabelFR(routeLongName);
 	}
 
-	private static final String AGENCY_COLOR = "1F1F1F"; // DARK GRAY (from GTFS)
-
-	@NotNull
 	@Override
-	public String getAgencyColor() {
-		return AGENCY_COLOR;
-	}
-
-	@Nullable
-	@Override
-	public String getRouteColor(@NotNull GRoute gRoute) {
-		if (StringUtils.isEmpty(gRoute.getRouteColor())) {
-			if ("1".equals(gRoute.getRouteShortName())) return "7E4A94";
-			if ("3".equals(gRoute.getRouteShortName())) return "41A62B";
-			if ("4".equals(gRoute.getRouteShortName())) return "EAC800";
-			if ("5".equals(gRoute.getRouteShortName())) return "E2007A";
-			if ("6".equals(gRoute.getRouteShortName())) return "B49C4C";
-			if ("7".equals(gRoute.getRouteShortName())) return "60BDDC";
-			if ("8".equals(gRoute.getRouteShortName())) return "F39400";
-			if ("T1".equals(gRoute.getRouteShortName())) return "E53834";
-			if ("T2".equals(gRoute.getRouteShortName())) return "007C79";
-			if ("T3".equals(gRoute.getRouteShortName())) return "79421E";
-			if ("325".equals(gRoute.getRouteShortName())) return "57585A";
-			if ("330".equals(gRoute.getRouteShortName())) return "57585A";
-			if ("340".equals(gRoute.getRouteShortName())) return "57585A";
-			if ("350".equals(gRoute.getRouteShortName())) return "57585A";
-			if ("600".equals(gRoute.getRouteShortName())) return "9C9E9F";
-			throw new MTLog.Fatal("Unexpected route color for %s!", gRoute);
-		}
-		return super.getRouteColor(gRoute);
+	public boolean defaultAgencyColorEnabled() {
+		return true;
 	}
 
 	@Override
 	public boolean directionFinderEnabled() {
 		return true;
 	}
-
-	private static final Pattern DIRECTION = Pattern.compile("(direction )", Pattern.CASE_INSENSITIVE);
 
 	private static final Pattern SERVICE_LOCAL = Pattern.compile("(service local)", Pattern.CASE_INSENSITIVE);
 
@@ -123,11 +89,11 @@ public class SteJulieOMITSJUBusAgencyTools extends DefaultAgencyTools {
 	@NotNull
 	@Override
 	public String cleanTripHeadsign(@NotNull String tripHeadsign) {
+		tripHeadsign = CleanUtils.keepToFR(tripHeadsign);
 		tripHeadsign = _DASH_.matcher(tripHeadsign).replaceAll(_DASH_REPLACEMENT); // from - to => form<>to
 		tripHeadsign = EXPRESS_.matcher(tripHeadsign).replaceAll(EMPTY);
 		tripHeadsign = DEVANT_.matcher(tripHeadsign).replaceAll(EMPTY);
 		tripHeadsign = MONT_SAINT_.matcher(tripHeadsign).replaceAll(MONT_SAINT_REPLACEMENT);
-		tripHeadsign = DIRECTION.matcher(tripHeadsign).replaceAll(EMPTY);
 		tripHeadsign = SERVICE_LOCAL.matcher(tripHeadsign).replaceAll(EMPTY);
 		tripHeadsign = CleanUtils.cleanBounds(Locale.FRENCH, tripHeadsign);
 		tripHeadsign = CleanUtils.cleanStreetTypesFRCA(tripHeadsign);
@@ -170,8 +136,6 @@ public class SteJulieOMITSJUBusAgencyTools extends DefaultAgencyTools {
 		}
 		return super.getStopCode(gStop);
 	}
-
-	private static final Pattern DIGITS = Pattern.compile("[\\d]+");
 
 	@Override
 	public int getStopId(@NotNull GStop gStop) {
